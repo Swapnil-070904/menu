b8ac97ece91d205dbcd24e70b9b218ca186465ac
Swapnil-070904 <ak6805002@gmail.com>
2024-08-01 16:04:29 +0000

    update

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import smtplib,os
+import smtplib
 from twilio.rest import Client
 from flask import Flask, request, render_template, redirect, url_for, flash
 
@@ -10,7 +10,9 @@ FROM_EMAIL = "ak6805002@gmail.com"
 FROM_PASSWORD = "lqby vkmp ykfg berh"
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
-
+account_sid = 'AC7ace71fd257c6ea8db0b126215487662'
+auth_token = '9871251fb007510d65ff52afe6027b66'
+number = '+15739282798'
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -23,6 +25,9 @@ def handle_action():
         return render_template('form.html')
     elif action == 'call':
         return render_template('call.html')
+    elif action == 'Sms':
+        return render_template('Sms.html')
+
     else:
         return 'Unknown action!'
 
@@ -50,7 +55,7 @@ Subject: {subject}
     except Exception as e:
         flash(f'Failed to send email: {e}')
     
-    return render_template('index.html')
+    return render_template('form.html')
 @app.route("/call", methods=['post'])
 def call():
     no= request.form['to']
@@ -58,7 +63,7 @@ def call():
     auth_token = "9871251fb007510d65ff52afe6027b66"
     client = Client(account_sid, auth_token)
     call = client.calls.create(
-            from_='+15739282798',
+            from_=number,
 
             to=no,
 
@@ -67,7 +72,20 @@ def call():
         )
 
     return(call.sid)
+@app.route("/Sms", methods=['post'])
+def Sms():
+    msg=request.form['msg']
+    no=request.form['to']
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+    body=msg,
 
+	    from_=number,
+
+	    to=no
+
+	)
+    return(f"Message sent with SID: {message.sid}")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port='5000')
